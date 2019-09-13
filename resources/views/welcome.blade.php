6a2d69056c19d45026e4d32a6c1060f5427c11f8
Peniel Dialu <dialundamapeniel@gmail.com>
2019-09-13 17:21:40 +0100

    Add diaporama dans la page welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,79 @@
 @extends('layouts.master')
+@section('stylesheet1')
+<script>
+
+  tbimage=new Array('img1','img2','img3','img4','img5','img6','img7','img8','img9','img10') //tableau des image mettre le nom de toutes les images
+  preImages=new Array
+  var opaa=0;
+  var opab=100;
+  var compteur=2
+  var vitesse=2000
+
+  function precharge() { 
+    for (i = 0; i < tbimage.length; i++) { 
+      preImages[i] = new Image()
+      preImages[i].src = "{!! asset('img') .'/' !!}"+tbimage[i]+".jpg"
+  }
+    setTimeout(defilmage,vitesse);
+  }
+
+  function defilmage(reg){
+    oxo=reg;
+    if(compteur==tbimage.length-1){
+    compteur=-1
+  }
+  if(oxo==1){
+    compteur++
+    opaa=10;
+    document.getElementById('divimageb').src="{!! asset('img/').'/' !!}"+tbimage[compteur]+".jpg";
+    oxo=0;
+  }
+  var imacibleb=document.getElementById('divimagea');
+  var imaciblea=document.getElementById('divimageb');
+
+  opaa+=3;
+  opab-=4;
+  if(document.all && !window.opera){ 
+    imaciblea.style.filter = 'alpha(opacity=' + opaa + ');' ;
+    imacibleb.style.filter = 'alpha(opacity=' + opab + ');';
+  } 
+  else{ 
+    imaciblea.style.opacity = opaa/100;
+    imacibleb.style.opacity = opab/100;
+  }
+  if(opaa>=100){
+    opaa=10;
+    opab=100;
+    var xcc=imaciblea.src.length-4
+    var cxx=imaciblea.src.lastIndexOf("/")+1
+    var nomimg=imaciblea.src.substring(cxx,xcc)
+    imacibleb.src='images/'+nomimg+'.jpg'
+    setTimeout("defilmage(1)",vitesse);
+    return false
+  }
+    setTimeout("defilmage()",25);
+  }
+  if(navigator.appName.substring(0,3)=="Mic"){
+    attachEvent("onload",precharge);
+  }
+  else{
+    addEventListener("load", precharge, false);
+  }
+</script>
+@endsection
 @section('content')
 <div class="row">
-    <div class="col-md-6"></div>               
+    <div class="col-md-6">
+      <div class="card">
+        {{-- <div class="card-header bg-danger">IMG</div> --}}
+        <div class="card-body " style="background-color: #f8f9fa">
+          <div id="divconteneur">
+          <img id='divimagea' class="img-fluid border rounded-circle" src="{{ asset('img/img1.jpg') }}" style="opacity:100;FILTER:alpha(opacity=100);">
+          <img id='divimageb'class="img-fluid rounded-circle" src="{{ asset('img/img2.jpg') }}" style="opacity:0;FILTER:alpha(opacity=0); min-height: 400px">
+          </div>
+        </div>  
+      </div>
+    </div>               
     <div class="col-md-1"></div>               
     <div class="col-md-5">
       @if ($errors->any())
@@ -47,3 +119,6 @@
     </div>               
 </div>
 @endsection
+@push('scripts')
+
+@endpush
